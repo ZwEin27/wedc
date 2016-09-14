dd08e94ac877620b7766bea6467f97ac3d0942b4
ZwEin <zwein27@gmail.com>
2016-09-13 19:04:21 -0700

    add w2v model

--- labelprop/labelprop.py
+++ labelprop/labelprop.py
@@ -2,11 +2,12 @@
 # @Author: ZwEin
 # @Date:   2016-09-13 14:44:46
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-09-13 16:07:21
+# @Last Modified time: 2016-09-13 19:03:41
 
 import os
 import sys
 import csv
+import w2v
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
@@ -17,11 +18,12 @@ class WEDC(object):
 
     def __init__(self, data_path):
         self.data_path = data_path
+        self.w2v_model_path = os.path.join(os.path.dirname(__file__), 'w2v_model.bin')
         self.corpus, self.labels = self.load_data()
         self.size = len(self.labels)
-        self.vectorizer = CountVectorizer(min_df=1)
-        self.classifier = KNeighborsClassifier(n_neighbors=5, metric='jaccard')
-        self.rs = cross_validation.ShuffleSplit(self.size, n_iter=1, test_size=.25, random_state=12)
+        # self.vectorizer = CountVectorizer(min_df=1)
+        # self.classifier = KNeighborsClassifier(n_neighbors=5, metric='jaccard')
+        # self.rs = cross_validation.ShuffleSplit(self.size, n_iter=1, test_size=.25, random_state=12)
 
     def load_data(self):
         dataset = []
@@ -36,9 +38,25 @@ class WEDC(object):
                 labels.append(label)
         return dataset, labels
 
+    def load(self):
+
+        w2v.setup_model(input, 
+                        output, 
+                        binary=1, 
+                        cbow=0, 
+                        size=300, 
+                        window=10, 
+                        negative=5, 
+                        hs=0, 
+                        threads=12, 
+                        iter_=5, 
+                        min_count=5, 
+                        verbose=False)
+
     def run(self):
         vectors = self.vectorizer.fit_transform(self.corpus).toarray()
         
+        """
         for train_index, test_index in self.rs:
             train_X = [vectors[i] for i in range(self.size) if i in train_index]
             train_y = [self.labels[i] for i in range(self.size) if i in train_index]
@@ -62,6 +80,7 @@ class WEDC(object):
                 print '# ', target_names[int(text_y[idx])-2], 'error predicted as', target_names[int(pred_y[idx])-2]
                 print '#'*60
                 print test_origin[idx]
+        """
 
 
 
