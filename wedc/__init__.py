01519af3770ef327a79f3302045b53fc7869c1b1
ZwEin <zwein27@gmail.com>
2016-09-24 13:59:22 -0700

    add plot

--- wedc/__init__.py
+++ wedc/__init__.py
@@ -2,14 +2,16 @@
 # @Author: ZwEin
 # @Date:   2016-09-23 12:58:37
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-09-23 14:00:59
+# @Last Modified time: 2016-09-24 13:59:03
 
 import os
 import sys
 import csv
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 from random import shuffle
 from sklearn.utils import resample
+from sklearn.metrics import precision_recall_curve
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
@@ -17,6 +19,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import cross_validation
 from sklearn.metrics import classification_report
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import average_precision_score
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import label_binarize
 
 ##################################################################
 # Constant
@@ -102,52 +107,133 @@ class WEDC(object):
         test_origin = [corpus[i] for i in range(size) if i in test_index]
 
         train_X = [vectors[i] for i in range(size) if i in train_index]
-        train_y = [labels[i] for i in range(size) if i in train_index]
+        train_y = [int(labels[i]) for i in range(size) if i in train_index]
         test_X = [vectors[i] for i in range(size) if i in test_index]
-        text_y = [labels[i] for i in range(size) if i in test_index]
+        text_y = [int(labels[i]) for i in range(size) if i in test_index]
 
         self.classifier.fit(train_X, train_y)
         pred_y = self.classifier.predict(test_X)
+        pred_proba_y = self.classifier.predict_proba(test_X)
 
-
-
-        # return corpus, labels, size, train_index, test_index
+        # print pred_y
+        # print pred_proba_y
+        
+        return text_y, pred_y, pred_proba_y
 
     def __run_split_train_test_data(self, train_test_split=.25, random_state=None, n_iter=1):
         corpus = self.corpus
         labels = self.labels
         size = self.size
 
-        for train_index, test_index in cross_validation.ShuffleSplit(size, n_iter=n_iter, test_size=train_test_split, random_state=random_state):
-            pass
+        vectors = self.vectorizer.fit_transform(corpus).toarray()
 
+        for train_index, test_index in cross_validation.ShuffleSplit(size, n_iter=n_iter, test_size=train_test_split, random_state=random_state):
+            
+            test_origin = [corpus[i] for i in range(size) if i in test_index]
+
+            train_X = [vectors[i] for i in range(size) if i in train_index]
+            train_y = [int(labels[i]) for i in range(size) if i in train_index]
+            test_X = [vectors[i] for i in range(size) if i in test_index]
+            text_y = [int(labels[i]) for i in range(size) if i in test_index]
+
+            self.classifier.fit(train_X, train_y)
+            pred_y = self.classifier.predict(test_X)
+            # pred_proba_y = self.classifier.predict_proba(test_X)
+        
+        return text_y, pred_y, None
 
     def run(self, train_data_path=None, test_data_path=None, train_test_split=.25, random_state=None, n_iter=1):
 
-        # corpus = []
-        # labels = []
-        # size = 0
-        # train_index = []
-        # test_index = []
-
         if train_data_path and test_data_path:
-            self.__run_specific_train_test_data(train_data_path, test_data_path)
+            y_test, y_pred, y_pred_proba = self.__run_specific_train_test_data(train_data_path, test_data_path)
         elif not train_data_path and not test_data_path:
-            self.__run_split_train_test_data(train_test_split=train_test_split, random_state=random_state, n_iter=n_iter)
+            y_test, y_pred, y_pred_proba = self.__run_split_train_test_data(train_test_split=train_test_split, random_state=random_state, n_iter=n_iter)
         else:
             raise Exception('incorrect format')
 
+        # self.plot(y_test, y_pred, y_pred_proba=y_pred_proba)
 
+
+
+
+    ##################################################################
+    # Statistic Report Methods
+    ##################################################################
+    
+
+
+    ##################################################################
+    # Plot Methods
+    ##################################################################
+
+    def plot(self, y_test, y_pred, y_pred_proba=None):
+        # print y_test
+        # print y_pred
+        
+        target_labels = [2, 3, 4]
+
+        y_test = np.array(y_test)
+        y_pred = np.array(y_pred)
+        y_pred_proba = np.array(y_pred_proba)
+
+        y_test = label_binarize(y_test, classes=target_labels)
+
+        precision = dict()
+        recall = dict()
+        average_precision = dict()
+
+        for i in range(len(target_labels)):
+            precision[i], recall[i], _ = precision_recall_curve(y_test[:, i],
+                                                                y_pred_proba[:, i])
+            average_precision[i] = average_precision_score(y_test[:, i], y_pred_proba[:, i])
+
+        # # Plot Precision-Recall curve
+        # plt.clf()
+        # plt.plot(recall[0], precision[0], label='Precision-Recall curve')
+        # plt.xlabel('Recall')
+        # plt.ylabel('Precision')
+        # plt.ylim([0.0, 1.05])
+        # plt.xlim([0.0, 1.0])
+        # plt.title('Precision-Recall example: AUC={0:0.2f}'.format(average_precision[0]))
+        # plt.legend(loc="lower left")
+        # plt.show()
+
+        # Plot Precision-Recall curve for each class
+        plt.clf()
+        # plt.plot(recall["micro"], precision["micro"],
+        #          label='micro-average Precision-recall curve (area = {0:0.2f})'
+        #                ''.format(average_precision["micro"]))
+        
+        for i in range(len(target_labels)):
+            plt.plot(recall[i], precision[i],
+                     label='Precision-recall curve of class {0} (area = {1:0.2f})'
+                           ''.format(i, average_precision[i]))
+
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title('Extension of Precision-Recall curve to multi-class')
+        plt.legend(loc="lower right")
+        plt.show()
+
+
+
+        
 
 
 if __name__ == '__main__':
-    dc = WEDC(vectorizer_type='count', classifier_type='knn')
+    
 
+    ## Train and test path provided separately
+    # dc = WEDC(vectorizer_type='count', classifier_type='knn')
+    # train_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/training-data/dig_memex_eval_datasets.csv'
+    # test_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/testing-data/testing_data.csv'
+    # dc.run(train_data_path=train_path, test_data_path=test_path)
 
-    train_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/training-data/dig_memex_eval_datasets.csv'
-    test_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/testing-data/testing_data.csv'
-    dc.run(train_data_path=train_path, test_data_path=test_path)
-
-        
+    ## Only one data path provided
+    data_path = '/Users/ZwEin/job_works/StudentWork_USC-ISI/projects/dig-groundtruth-data/classification/testing-data/testing_data.csv'
+    dc = WEDC(data_path=data_path, vectorizer_type='count', classifier_type='knn')
+    dc.run(train_test_split=.25, random_state=None, n_iter=1)
 
 
